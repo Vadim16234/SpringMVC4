25199e2b9d18593f574963f65ee675fcd24f6b58
Vadik <gloom16@mail.ru>
2023-12-07 02:40:52 +0300

    добавил аннотации @RequestMapping - для указания адреса. @RequestParam - указание параметров в адресной строке

--- src/main/java/lesson4/controllers/FirstController.java
+++ src/main/java/lesson4/controllers/FirstController.java
@@ -1,14 +1,20 @@
 package lesson4.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
